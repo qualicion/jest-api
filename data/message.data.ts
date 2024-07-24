8b689db6771e6d7486addde6d13d3d65bc069dad
Babatunde Oduniyi <qualicion@outlook.com>
2024-07-24 09:27:46 +0100

    updated message data file

--- data/message.data.ts
+++ data/message.data.ts
@@ -1,11 +1,16 @@
-export const brandUpdateErrorMessage = () => ({
+export const brandUpdateErrorMessage = {
   error: "Unable to update brands",
-});
+};
 
-export const brandTooLongErrorMessage = () => ({
+export const brandTooLongErrorMessage = {
   error: "Brand name is too long",
-});
+};
 
-export const brandDeleteInvalidErrorMessage = () => ({
+export const brandDeleteInvalidErrorMessage = {
   error: "Unable to delete brand",
-});
+};
+
+export const brandNotFoundErrorMessage = {
+  error: "Brand not found.",
+};
+
