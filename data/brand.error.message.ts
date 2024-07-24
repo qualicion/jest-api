d8a809fb963f21fc37ef0ccb0ea3cebda1448280
Babatunde Oduniyi <qualicion@outlook.com>
2024-07-24 12:29:00 +0100

    changed error message file

---
+++ data/brand.error.message.ts
@@ -0,0 +1,16 @@
+export const brandUpdateErrorMessage = {
+  error: "Unable to update brands",
+};
+
+export const brandTooLongErrorMessage = {
+  error: "Brand name is too long",
+};
+
+export const brandDeleteInvalidErrorMessage = {
+  error: "Unable to delete brand",
+};
+
+export const brandNotFoundErrorMessage = {
+  error: "Brand not found.",
+};
+
