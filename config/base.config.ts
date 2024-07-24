e19026e75974143c58ddbf46a7d3b1925b58b13b
Babatunde Oduniyi <qualicion@outlook.com>
2024-07-24 07:58:33 +0100

    removed unrequired data

--- config/base.config.ts
+++ config/base.config.ts
@@ -1,6 +1,3 @@
 export default {
   baseUrl: "https://practice-react.sdetunicorns.com/api/test",
-  email: "mod@mail.com",
-  password: "Modpass123!",
-  invalidBrandId: ["643b117fbf61a969820eb7cc", '643b117fbf61a969820eb7c2' ] 
 };
