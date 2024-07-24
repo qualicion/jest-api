a9d4069e78c2fa3fcb121b9c54027f7a04aaff04
Babatunde Oduniyi <qualicion@outlook.com>
2024-07-24 08:45:55 +0100

    updated test data file

--- data/brand.data.ts
+++ data/brand.data.ts
@@ -2,3 +2,27 @@ export default {
   invalidBrandId: ["643b117fbf61a969820eb7c2", "643fc976bf61a969820eb7boo"],
   unavailableBrandId: ["643b117fbf61a969820eb7cc"],
 };
+
+export const generateBrandData = () => ({
+  name: "Test Brand" + Math.floor(Math.random() * 100000),
+  description: "Test Brand Description",
+});
+
+export const emptyBrandData = () => ({
+  name: "",
+  description: "Test Brand Description",
+});
+
+export const minimumBrandData = () => ({
+  name: "a",
+  description: "Test Brand Description",
+});
+
+export const longStringBrandData = () => ({
+  name: "This is a string that is definitely over thirty characters long.",
+});
+
+
+export const invalidBrandData = () => ({
+  name: "invalid brand",
+});
