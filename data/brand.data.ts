5122a67775820793dbe20deddeefd08f25c6ed04
Babatunde Oduniyi <qualicion@outlook.com>
2024-07-24 09:27:24 +0100

    update brand data file

--- data/brand.data.ts
+++ data/brand.data.ts
@@ -8,21 +8,28 @@ export const generateBrandData = () => ({
   description: "Test Brand Description",
 });
 
-export const emptyBrandData = () => ({
-  name: "",
-  description: "Test Brand Description",
-});
+export const emptyBrandData = [
+  {
+    name: "",
+    description: "Test Brand Description",
+  },
+];
 
-export const minimumBrandData = () => ({
-  name: "a",
-  description: "Test Brand Description",
-});
+export const minimumBrandData = [
+  {
+    name: "a",
+    description: "Test Brand Description",
+  },
+];
 
-export const longStringBrandData = () => ({
-  name: "This is a string that is definitely over thirty characters long.",
-});
+export const longStringBrandData = [
+  {
+    name: "This is a string that is definitely over thirty characters long.",
+  },
+];
 
-
-export const invalidBrandData = () => ({
-  name: "invalid brand",
-});
+export const invalidBrandData = [
+  {
+    name: "invalid brand",
+  },
+];
